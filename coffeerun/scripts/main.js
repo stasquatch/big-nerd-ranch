d46258586237111262f9195891dac71729a55e0a
Stacy Harrison <Stacy Harrison>
2017-02-08 19:25:56 -0500

    first commit

---
+++ coffeerun/scripts/main.js
@@ -0,0 +1,26 @@
+(function(window) {
+  'use strict';
+  var FORM_SELECTOR = '[data-coffee-order="form"]';
+  var CHECKLIST_SELECTOR = '[data-coffee-order="checklist"]';
+  var SERVER_URL = 'https://coffeerun-v2-rest-api.herokuapp.com/api/coffeeorders';
+  var App = window.App;
+  var Truck = App.Truck;
+  var DataStore = App.DataStore;
+  var RemoteDataStore = App.RemoteDataStore;
+  var FormHandler = App.FormHandler;
+  var Validation = App.Validation;
+  var CheckList = App.CheckList;
+  var remoteDS = new RemoteDataStore(SERVER_URL);
+  var myTruck = new Truck('ncc-1701', remoteDS);
+  window.myTruck = myTruck;
+  var checkList = new CheckList(CHECKLIST_SELECTOR);
+  checkList.addClickHandler(myTruck.deliverOrder.bind(myTruck));
+  var formHandler = new FormHandler(FORM_SELECTOR);
+
+  formHandler.addSubmitHandler(function(data) {
+    myTruck.createOrder.call(myTruck, data);
+    checkList.addRow.call(checkList, data);
+  });
+
+  formHandler.addInputHandler(Validation.isCompanyEmail);
+})(window);
