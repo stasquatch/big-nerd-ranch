b9e8913f3236c030944b3c1dd10106f706801a4a
Stacy Harrison <stacy.e.harrison@gmail.com>
2017-04-19 09:22:38 -0400

    remove submodule

---
+++ tracker/app/models/witness.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+  fName: DS.attr('string'),
+  lName: DS.attr('string'),
+  email: DS.attr('string'),
+  sightings: DS.hasMany('sighting'),
+  fullName: Ember.computed('fName', 'lName', function() {
+    return this.get('fName') + ' ' + this.get('lName');
+  })
+});
